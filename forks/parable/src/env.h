55e94965930a4de8dbc8707542eddcc713b4113a
clarking <clarkaaron@hotmail.com>
2022-09-04 04:01:49 -0700

    cmake update for parable

---
+++ forks/parable/src/env.h
@@ -0,0 +1,34 @@
+/************************************************************************
+ * Parable, a little smalltalk
+ *
+ * This code is gifted to the public domain. Share freely.
+ *
+ * This include file gathers together environmental factors that are
+ * likely to change from one C compiler to another, or from one system
+ * to another.
+ ************************************************************************/
+
+/* define custom data types */
+typedef int boolean;
+typedef unsigned char byte;
+
+#define true 1
+#define false 0
+
+#define byteToInt(b) (b)
+#define longCanBeInt(l) ((l >= -16383) && (l <= 16383))
+#define streq(a, b) (strcmp(a, b) == 0)
+#define ignore (void)
+#define noreturn void
+
+/* Prototypes */
+#define X ,
+#define OBJ object
+#define OBJP object *
+#define INT int
+#define BOOL boolean
+#define STR char *
+#define FLOAT double
+#define NOARGS void
+#define FILEP FILE *
+#define FUNC ()
---
+++ forks/parable/src/lex.h
@@ -0,0 +1,30 @@
+/************************************************************************
+ * Parable, a little smalltalk
+ *
+ * This code is gifted to the public domain. Share freely.
+ *
+ * Values returned by the lexical analyzer.
+ ************************************************************************/
+
+typedef enum tokensyms {
+  nothing,
+  nameconst,
+  namecolon,
+  intconst,
+  floatconst,
+  charconst,
+  symconst,
+  arraybegin,
+  strconst,
+  binary,
+  closing,
+  inputend
+} tokentype;
+
+extern tokentype nextToken(NOARGS);
+
+extern tokentype token;    /* token variety                         */
+extern char tokenString[]; /* text of current token                 */
+extern int tokenInteger;   /* integer (or character) value of token */
+extern double tokenFloat;  /* floating point value of token         */
+extern noreturn lexinit(); /* initialization routine                */
